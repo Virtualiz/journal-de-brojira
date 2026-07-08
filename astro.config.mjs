8505bcd33d945e33c2693de98c2aa48716c61a4a
Bruno Sinteff <bruno.sinteff@ekino.com>
2026-07-08 16:06:49 +0200

    feat: add Tailwind, DaisyUI and svelte integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({ site: "https://example.com" });
+export default defineConfig({
+  site: "https://journal-de-brojira.netlify.app/",
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
